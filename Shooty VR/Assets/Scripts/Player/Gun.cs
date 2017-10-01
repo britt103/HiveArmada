77bbfe351bf13823875c468b28a68e743c475cc8
Perry Sidler <sidle104@mail.chapman.edu>
2017-09-30 21:02:42 -0700

    begin migration from VRTK to SteamVR

---
+++ Shooty VR/Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,27 @@
+﻿//=============================================================================
+// 
+// Perry Sidler
+// 1831784
+// sidle104@mail.chapman.edu
+// CPSC-340-01 & CPSC-344-01
+// Group Project
+// 
+// [DESCRIPTION]
+// 
+//=============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameName.Player
+{
+    public abstract class Gun : MonoBehaviour
+    {
+        public int damage;
+        public float fireRate;
+        protected bool canShoot = true;
+
+        public abstract void TriggerUpdate();
+    }
+}
---
+++ Shooty VR/Assets/Scripts/Player/LaserGun.cs
@@ -0,0 +1,30 @@
+﻿//=============================================================================
+// 
+// Perry Sidler
+// 1831784
+// sidle104@mail.chapman.edu
+// CPSC-340-01 & CPSC-344-01
+// Group Project
+// 
+// [DESCRIPTION]
+// 
+//=============================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameName;
+using System;
+
+namespace GameName.Player
+{
+    public class LaserGun : Gun
+    {
+        /// <summary>
+        /// Sent every frame while the trigger is pressed
+        /// </summary>
+        public override void TriggerUpdate()
+        {
+            
+        }
+    }
+}

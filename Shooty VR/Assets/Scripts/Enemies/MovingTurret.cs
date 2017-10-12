7e455e97c3b118285057a52c2ff172f8b3f828b4
Ryan <britt103@mail.chapman.edu>
2017-10-12 16:35:14 -0700

    Change MovingTurret to Sin movement

--- Shooty VR/Assets/Scripts/Enemies/MovingTurret.cs
+++ Shooty VR/Assets/Scripts/Enemies/MovingTurret.cs
@@ -11,70 +11,57 @@ using UnityEngine;
 /// 
 /// This script handles the logic of a turret moving between two points
 /// </summary>
-
-public class MovingTurret : MonoBehaviour
+namespace ShootyVR.Enemies
 {
-    public float movingSpeed;
-    public float xMax;    
-    public float yMax;
-    private Vector3 posA;
-    private Vector3 posB;
-    private bool perry = false;
-    private float xEnd;
-    public GameObject bullet;
-    public Transform spawn;
-    GameObject player;
-    public Vector3 pos;
-    public float fireRate, fireSpeed;
-    private float fireNext;
-    bool canFire;
-    private float distance;
-
-
-    // Use this for initialization
-    void Start()
+    public class MovingTurret : EnemyBasic
     {
-        SetPosition(); 
-        player = GameObject.FindGameObjectWithTag("Player");
-        
-    }
-    void SetPosition()
-    {
-        pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        posA = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        posB = new Vector3(transform.position.x + xMax,transform.position.y + yMax, 0);
-    }
-    private void Update()
-    {
-        pos = transform.position;
-        float step = movingSpeed * Time.deltaTime;
-        if (perry == false) {
-            Debug.Log("I moved, senpai!");
-            transform.position = Vector3.MoveTowards(pos, posB, step);
-        }
-        else
+        public float movingSpeed;
+        public float xMax;
+        public float yMax;
+        private Vector3 posA;
+        private Vector3 posB;
+        private Vector3 posCenter;
+        private bool perry = false;
+        private float xEnd;
+        public GameObject bullet;
+        public Transform spawn;
+        GameObject player;
+        public Vector3 pos;
+        public float fireRate, fireSpeed;
+        private float fireNext;
+        bool canFire;
+        private float distance;
+
+
+        // Use this for initialization
+        void Start()
         {
-            Debug.Log("Die senpai!");
-            transform.position = Vector3.MoveTowards(pos, posA, step);
+            SetPosition();
+            player = GameObject.FindGameObjectWithTag("Player");
+
         }
-        
-        if (pos.Equals(posA))
+        void SetPosition()
         {
-            perry = false;
+            //This sets the starting position and the 2 points the turrets moves between
+            pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            posA = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            posB = new Vector3(transform.position.x + xMax, transform.position.y + yMax, 0);
+            posCenter = new Vector3(posA.x + posB.x, posA.y + posB.y) / 2.0f;
+            transform.position = posCenter;
+
         }
-        else if (pos.Equals(posB))
+        private void Update()
         {
-            perry = true;
+            pos = transform.position;
+            transform.position = Vector3.Lerp(posA, posB, (Mathf.Sin(movingSpeed * Time.time) + 1.0f) / 2.0f);
+          
+            /*if (Time.time > fireNext)
+            {
+                fireNext = Time.time + fireRate;
+                var shoot = Instantiate(bullet, spawn.position, spawn.rotation);
+                shoot.GetComponent<Rigidbody>().velocity = shoot.transform.forward * fireSpeed;
+            }*/
         }
 
-        transform.LookAt(pos);
-
-        /*if (Time.time > fireNext)
-        {
-            fireNext = Time.time + fireRate;
-            var shoot = Instantiate(bullet, spawn.position, spawn.rotation);
-            shoot.GetComponent<Rigidbody>().velocity = shoot.transform.forward * fireSpeed;
-        }*/
     }
-
 }

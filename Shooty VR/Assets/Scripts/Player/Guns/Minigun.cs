c9a4e488c2899b912d02c511852ce4a6a01baed1
Perry Sidler <sidle104@mail.chapman.edu>
2017-11-01 15:13:33 -0700

    add documentation to Minigun, add functions to Minigun for tracers and hit sparks

--- Shooty VR/Assets/Scripts/Player/Guns/Minigun.cs
+++ Shooty VR/Assets/Scripts/Player/Guns/Minigun.cs
@@ -6,20 +6,25 @@
 // CPSC-340-01 & CPSC-344-01
 // Group Project
 // 
-// [DESCRIPTION]
+// This class handles the miniguns for the player ship. Each minigun has 6
+// barrels. It alternates between which minigun is shooting and randomly picks
+// a barrel to shoot from. Every other shot (barrel-dependent) will spawn a hit
+// spark effect at shot target, either the enemy or the wall. The minigun will
+// also periodically shoot a visible tracer effect to give the player more
+// feedback so they know where they are shooting.
 // 
 //=============================================================================
 
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Hive.Armada;
 using Hive.Armada.Enemies;
-using UnityEngine.Rendering;
 
 namespace Hive.Armada.Player.Guns
 {
     /// <summary>
-    /// 
+    /// The minigun controller for the player ship.
     /// </summary>
     public class Minigun : Gun
     {
@@ -56,15 +61,17 @@ namespace Hive.Armada.Player.Guns
         /// </summary>
         private bool isLeftFire = true;
 
-        public LineRenderer[] leftTracers;
-        public LineRenderer[] rightTracers;
-        [Tooltip("View makes line face camera. Local makes the line face the direction of the transform component")]
-        public LineAlignment alignment;
+        private LineRenderer[] leftTracers;
+        private LineRenderer[] rightTracers;
         public float thickness = 0.002f;
-        public ShadowCastingMode castShadows;
-        public bool receiveShadows = false;
         public Material tracerMaterial;
 
+        private bool leftSpark = true;
+        private bool rightSpark = false;
+
+        /// <summary>
+        /// Initializes variables
+        /// </summary>
         void Start()
         {
             leftTracers = new LineRenderer[left.Length];
@@ -74,18 +81,18 @@ namespace Hive.Armada.Player.Guns
             {
                 leftTracers[i] = left[i].AddComponent<LineRenderer>();
                 leftTracers[i].material = tracerMaterial;
-                leftTracers[i].shadowCastingMode = castShadows;
-                leftTracers[i].receiveShadows = receiveShadows;
-                leftTracers[i].alignment = alignment;
+                leftTracers[i].shadowCastingMode = ShadowCastingMode.Off;
+                leftTracers[i].receiveShadows = false;
+                leftTracers[i].alignment = LineAlignment.View;
                 leftTracers[i].startWidth = thickness;
                 leftTracers[i].endWidth = thickness;
                 leftTracers[i].enabled = false;
 
                 rightTracers[i] = right[i].AddComponent<LineRenderer>();
                 rightTracers[i].material = tracerMaterial;
-                rightTracers[i].shadowCastingMode = castShadows;
-                rightTracers[i].receiveShadows = receiveShadows;
-                rightTracers[i].alignment = alignment;
+                rightTracers[i].shadowCastingMode = ShadowCastingMode.Off;
+                rightTracers[i].receiveShadows = false;
+                rightTracers[i].alignment = LineAlignment.View;
                 rightTracers[i].startWidth = thickness;
                 rightTracers[i].endWidth = thickness;
                 rightTracers[i].enabled = false;
@@ -114,12 +121,12 @@ namespace Hive.Armada.Player.Guns
             RaycastHit hit;
             if (Physics.SphereCast(transform.position, radius, transform.forward, out hit, 200.0f, Utility.enemyMask))
             {
-                StartCoroutine(Shoot(hit.collider.gameObject.transform.position));
+                StartCoroutine(Shoot(hit.collider.gameObject, hit.collider.gameObject.transform.position));
 
-                //if (hit.collider.gameObject.GetComponent<EnemyBasic>() != null)
-                //{
-                //    hit.collider.gameObject.GetComponent<EnemyBasic>().Hit(damage);
-                //}
+                if (hit.collider.gameObject.GetComponent<Enemy>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Enemy>().Hit(damage);
+                }
             }
 
             //else if (Physics.SphereCast(transform.position, radius, transform.forward, out hit, 200.0F, Utility.uiMask))
@@ -135,45 +142,104 @@ namespace Hive.Armada.Player.Guns
 
             else if (Physics.Raycast(transform.position, transform.forward, out hit, 200.0f, Utility.roomMask))
             {
-                StartCoroutine(Shoot(hit.point));
+                StartCoroutine(Shoot(hit.collider.gameObject, hit.point));
             }
 
         }
 
         /// <summary>
-        /// 
+        /// Shoots the miniguns. Alternates between left and right, shooting from a
+        /// random barrel for aesthetic purposes. Every other shot from each minigun
+        /// will spawn a hit spark on the target. Periodically spawns a tracer to
+        /// give the player feedback as to where they are actually shooting.
         /// </summary>
-        /// <param name="target">  </param>
-        private IEnumerator Shoot(Vector3 target)
+        /// <param name="target"> The GameObject being shot </param>
+        /// <param name="position"> The position of the shot </param>
+        private IEnumerator Shoot(GameObject target, Vector3 position)
         {
             canShoot = false;
 
-            GameObject bullet = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            //GameObject bullet = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            //if (isLeftFire)
+            //{
+            //    bullet.transform.position = left[Random.Range(0, left.Length)].transform.position;
+            //}
+            //else
+            //{
+            //    bullet.transform.position = right[Random.Range(0, left.Length)].transform.position;
+            //}
+
+            //if (bullet.GetComponentInChildren<MinigunBullet>() != null)
+            //    bullet.GetComponentInChildren<MinigunBullet>().hand = shipController.hand;
+
+            //bullet.GetComponent<MinigunBullet>().damage = shipController.minigunDamage;
+            //bullet.transform.LookAt(target);
+
+            //if (bullet.GetComponentInChildren<Rigidbody>() != null)
+            //{
+            //    bullet.GetComponentInChildren<Rigidbody>().velocity = bullet.transform.forward * projectileSpeed;
+            //}
 
             if (isLeftFire)
             {
-                bullet.transform.position = left[Random.Range(0, left.Length)].transform.position;
+                // left[Random.Range(0, left.Length)].transform.position;
+                // do muzzle flash
+                if (leftSpark)
+                {
+                    StartCoroutine(HitSpark(target, 0.1f));
+                }
+
+                leftSpark = !leftSpark;
             }
             else
             {
-                bullet.transform.position = right[Random.Range(0, left.Length)].transform.position;
-            }
+                // right[Random.Range(0, right.Length)].transform.position;
+                // do muzzle flash
+                if (rightSpark)
+                {
+                    // do hit spark
+                }
 
-            if (bullet.GetComponentInChildren<MinigunBullet>() != null)
-                bullet.GetComponentInChildren<MinigunBullet>().hand = shipController.hand;
-
-            bullet.GetComponent<MinigunBullet>().damage = shipController.minigunDamage;
-            bullet.transform.LookAt(target);
-
-            if (bullet.GetComponentInChildren<Rigidbody>() != null)
-            {
-                bullet.GetComponentInChildren<Rigidbody>().velocity = bullet.transform.forward * projectileSpeed;
+                rightSpark = !rightSpark;
             }
 
             yield return new WaitForSeconds(1.0f / fireRate);
 
             isLeftFire = !isLeftFire;
             canShoot = true;
+        }
+
+        /// <summary>
+        /// Calculates the delay for a "bullet" to "travel"
+        /// from the minigun to the target enemy.
+        /// </summary>
+        /// <returns></returns>
+        private float CalculateDelay()
+        {
+            return 0.0f;
+        }
+
+        /// <summary>
+        /// Spawns a hit spark on target after a delay to emulate bullet travel.
+        /// </summary>
+        /// <param name="target"> The GameObject that is being hit </param>
+        /// <param name="delay"> How long to wait before spawning the hit spark </param>
+        private IEnumerator HitSpark(GameObject target, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            // spawn the spark on target
+        }
+
+        /// <summary>
+        /// Shoots a tracer at the target position
+        /// </summary>
+        /// <param name="barrel"> Which barrel to spawn the tracer from </param>
+        /// <param name="target"> Where to aim the tracer </param>
+        private IEnumerator Tracer(GameObject barrel, Vector3 target)
+        {
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }

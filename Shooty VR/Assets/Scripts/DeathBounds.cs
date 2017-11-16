a61b67646b86030ce9a2ca23820a052cc64e3875
Chad Johnson <cjohnson92130@gmail.com>
2017-11-16 11:09:18 -0800

    Add player death bounds with current functionality of resetting the scene, add easy-to-reach test bound.

---
+++ Shooty VR/Assets/Scripts/DeathBounds.cs
@@ -0,0 +1,34 @@
+﻿//=============================================================================
+//
+// Chad Johnson
+// 1763718
+// johns428@mail.chapman.edu
+// CPSC-340-01 & CPSC-344-01
+// Group Project
+//
+// DeathBounds penalizes the player for colliding with the Death Bounds
+// GameObject. 
+//
+//=============================================================================
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Penalizes player for leaving play area.
+/// </summary>
+public class DeathBounds : MonoBehaviour
+{
+    /// <summary>
+    /// Penalize player when FollowHead collides with bounds.
+    /// </summary>
+    /// <param name="other">Collider of object with which this collided.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "FollowHead")
+        {
+            Debug.Log("Out of Bounds.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}

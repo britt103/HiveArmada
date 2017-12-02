4915b95dfd750b1321aea29dd50c3b75ce1a8074
Perry Sidler <sidle104@mail.chapman.edu>
2017-12-02 13:03:16 -0800

    add setting of combat data for splitter children to SplitterTurret

--- Shooty VR/Assets/Scripts/Enemies/SplitterTurret.cs
+++ Shooty VR/Assets/Scripts/Enemies/SplitterTurret.cs
@@ -130,14 +130,34 @@ namespace Hive.Armada.Enemies
 
                 //Instantiate("Explosion.name", transform.position, transform.rotation); Placeholder for destroy effect
                 GameObject child1 = objectPoolManager.Spawn(typeIdentifier, transform.position, transform.rotation);
+                Enemy child1Enemy = child1.GetComponent<Enemy>();
+                Game.EnemySpawn childEnemySpawn = new Game.EnemySpawn(typeIdentifier, enemySpawn.spawnZone, enemySpawn.attackPattern);
+                child1Enemy.SetSubwave(subwave);
+                child1Enemy.SetEnemySpawn(childEnemySpawn);
+                child1Enemy.SetAttackPattern(enemySpawn.attackPattern);
+
                 GameObject child2 = objectPoolManager.Spawn(typeIdentifier, transform.position, transform.rotation);
+                Enemy child2Enemy = child1.GetComponent<Enemy>();
+                child2Enemy.SetSubwave(subwave);
+                child2Enemy.SetEnemySpawn(childEnemySpawn);
+                child2Enemy.SetAttackPattern(enemySpawn.attackPattern);
+
                 GameObject child3 = objectPoolManager.Spawn(typeIdentifier, transform.position, transform.rotation);
+                Enemy child3Enemy = child1.GetComponent<Enemy>();
+                child3Enemy.SetSubwave(subwave);
+                child3Enemy.SetEnemySpawn(childEnemySpawn);
+                child3Enemy.SetAttackPattern(enemySpawn.attackPattern);
+
                 GameObject child4 = objectPoolManager.Spawn(typeIdentifier, transform.position, transform.rotation);
-                
-                iTween.MoveTo(child1, iTween.Hash("x", transform.localPosition.x + (splitDir), "y", transform.localPosition.y + (splitDir), "z", transform.localPosition.z, "islocal", true, "time", 3.0f));
-                iTween.MoveTo(child2, iTween.Hash("x", transform.localPosition.x + (splitDir), "y", transform.localPosition.y - (splitDir), "z", transform.localPosition.z, "islocal", true, "time", 3.0f));
-                iTween.MoveTo(child3, iTween.Hash("x", transform.localPosition.x - (splitDir), "y", transform.localPosition.y + (splitDir), "z", transform.localPosition.z, "islocal", true, "time", 3.0f));
-                iTween.MoveTo(child4, iTween.Hash("x", transform.localPosition.x - (splitDir), "y", transform.localPosition.y - (splitDir), "z", transform.localPosition.z, "islocal", true, "time", 3.0f));
+                Enemy child4Enemy = child1.GetComponent<Enemy>();
+                child4Enemy.SetSubwave(subwave);
+                child4Enemy.SetEnemySpawn(childEnemySpawn);
+                child4Enemy.SetAttackPattern(enemySpawn.attackPattern);
+
+                iTween.MoveTo(child1, iTween.Hash("x", transform.localPosition.x + (splitDir), "y", transform.localPosition.y + (splitDir), "z", transform.localPosition.z, "islocal", false, "time", 3.0f));
+                iTween.MoveTo(child2, iTween.Hash("x", transform.localPosition.x + (splitDir), "y", transform.localPosition.y - (splitDir), "z", transform.localPosition.z, "islocal", false, "time", 3.0f));
+                iTween.MoveTo(child3, iTween.Hash("x", transform.localPosition.x - (splitDir), "y", transform.localPosition.y + (splitDir), "z", transform.localPosition.z, "islocal", false, "time", 3.0f));
+                iTween.MoveTo(child4, iTween.Hash("x", transform.localPosition.x - (splitDir), "y", transform.localPosition.y - (splitDir), "z", transform.localPosition.z, "islocal", false, "time", 3.0f));
             }
 
             Destroy(gameObject);

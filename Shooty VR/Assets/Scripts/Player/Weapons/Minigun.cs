cf463d2ca55c91ffe331561abf24c18912627fee
Perry Sidler <sidle104@mail.chapman.eduD>
2017-12-11 23:37:36 -0800

    add muzzle flash emitter pooling, cleanup code, update old comments, rename SetupLineRenderers() to generalized SetupWeapon()

--- Shooty VR/Assets/Scripts/Player/Weapons/Minigun.cs
+++ Shooty VR/Assets/Scripts/Player/Weapons/Minigun.cs
@@ -6,12 +6,12 @@
 // CPSC-340-01 & CPSC-344-01
 // Group Project
 // 
-// This class handles the miniguns for the player ship. Each minigun has 6
-// barrels. It alternates between which minigun is shooting and randomly picks
-// a barrel to shoot from. Every other shot (barrel-dependent) will spawn a hit
-// spark effect at shot target, either the enemy or the wall. The minigun will
-// also periodically shoot a visible tracer effect to give the player more
-// feedback so they know where they are shooting.
+// This class handles the miniguns for the player ship. Each minigun has 8
+// barrels. It alternates between left and right minigun, picking a random
+// barrel on the shooting minigun to shoot from. The minigun "shoots" a tracer
+// by flashing a LineRenderer. There is a muzzle flash and hit spark particle
+// emitter that spawn on the barrel and hit enemy, respectively, to give the
+// player more feedback.
 // 
 //=============================================================================
 
@@ -20,6 +20,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using Hive.Armada.Enemies;
+using MirzaBeig.ParticleSystems;
 using Random = UnityEngine.Random;
 
 namespace Hive.Armada.Player.Weapons
@@ -47,14 +48,16 @@ namespace Hive.Armada.Player.Weapons
 
         /// <summary>
         /// Array of points where the left minigun can shoot from.
+        /// These are the barrels on the left minigun.
         /// </summary>
-        [Tooltip("Points where the left minigun can shoot from.")]
+        [Tooltip("Array of points representing each barrel of the left minigun.")]
         public GameObject[] left;
 
         /// <summary>
         /// Array of points where the left minigun can shoot from.
+        /// These are the barrels on the right minigun.
         /// </summary>
-        [Tooltip("Points where the right minigun can shoot from.")]
+        [Tooltip("Array of points representing each barrel of the right minigun.")]
         public GameObject[] right;
 
         /// <summary>
@@ -62,6 +65,21 @@ namespace Hive.Armada.Player.Weapons
         /// </summary>
         [Header("Emitters")]
         public GameObject hitSparkEmitter;
+
+        /// <summary>
+        /// Particle emitter for the muzzle flash effect.
+        /// </summary>
+        public GameObject muzzleFlashEmitter;
+
+        /// <summary>
+        /// Pool of muzzle flash emitters.
+        /// </summary>
+        private ParticleSystems[] muzzleFlashEmitters;
+
+        /// <summary>
+        /// Index of the next muzzle flash emitter to use.
+        /// </summary>
+        private int muzzleFlashIndex;
 
         /// <summary>
         /// The audio source for the minigun sounds
@@ -72,10 +90,37 @@ namespace Hive.Armada.Player.Weapons
         /// <summary>
         /// The sound the minigun makes when it fires.
         /// </summary>
-        public AudioClip[] minigunShootSound;
+        public AudioClip minigunShootSound;
 
         /// <summary>
-        /// Gets enemy or wall aimpoint and shoots at it.
+        /// Initializes the LineRenderer's for the minigun tracers and
+        /// the muzzle flash emitter pool.
+        /// </summary>
+        protected override void SetupWeapon()
+        {
+            for (int i = 0; i < left.Length; ++i)
+            {
+                InitLineRenderer(left[i]);
+                InitLineRenderer(right[i]);
+            }
+
+            muzzleFlashEmitters = new ParticleSystems[Mathf.FloorToInt(fireRate * 2.5f)];
+            muzzleFlashIndex = 0;
+
+            for (int i = 0; i < muzzleFlashEmitters.Length; ++i)
+            {
+                GameObject muzzleFlash = Instantiate(muzzleFlashEmitter, transform);
+                ParticleSystems muzzleFlashSystem = muzzleFlash.GetComponent<ParticleSystems>();
+
+                muzzleFlashSystem.stop();
+                muzzleFlashSystem.clear();
+                muzzleFlashEmitters[i] = muzzleFlashSystem;
+            }
+        }
+
+        /// <summary>
+        /// Gets the enemy, interactable, or position on the wall that player is aimed at.
+        /// Shoots the enemy or interactable if there is one.
         /// </summary>
         protected override void Clicked()
         {
@@ -83,22 +128,25 @@ namespace Hive.Armada.Player.Weapons
             if (Physics.SphereCast(transform.position, radius, transform.forward, out hit, 200.0f,
                                    Utility.enemyMask))
             {
-                StartCoroutine(Shoot(hit.collider.gameObject, hit.point));
+                StartCoroutine(Shoot(hit.point));
+
+                Instantiate(hitSparkEmitter, hit.point,
+                            Quaternion.LookRotation(hit.point - gameObject.transform.position));
 
                 if (hit.collider.gameObject.GetComponent<Enemy>() != null)
                 {
                     hit.collider.gameObject.GetComponent<Enemy>().Hit(damage * damageMultiplier);
                 }
 
-                Instantiate(hitSparkEmitter, hit.point, Quaternion.LookRotation(hit.point - gameObject.transform.position));
-                //Instantiate(hitSparkEmitter, hit.point, Quaternion.LookRotation(hit.normal));
-
                 shipController.hand.controller.TriggerHapticPulse(2500);
             }
             else if (Physics.Raycast(transform.position, transform.forward, out hit, 200.0f,
                                      Utility.shootableMask))
             {
-                StartCoroutine(Shoot(hit.collider.gameObject, hit.point));
+                StartCoroutine(Shoot(hit.point));
+
+                Instantiate(hitSparkEmitter, hit.point,
+                            Quaternion.LookRotation(hit.point - gameObject.transform.position));
 
                 if (hit.collider.gameObject.GetComponent<Shootable>() != null
                     && hit.collider.gameObject.GetComponent<Shootable>().isShootable)
@@ -106,37 +154,41 @@ namespace Hive.Armada.Player.Weapons
                     hit.collider.gameObject.GetComponent<Shootable>().Shot();
                 }
 
-                Instantiate(hitSparkEmitter, hit.point, Quaternion.LookRotation(hit.point - gameObject.transform.position));
-                //Instantiate(hitSparkEmitter, hit.point, Quaternion.LookRotation(hit.normal));
-
                 shipController.hand.controller.TriggerHapticPulse(2500);
             }
             else if (Physics.Raycast(transform.position, transform.forward, out hit, 200.0f,
                                      Utility.roomMask))
             {
-                StartCoroutine(Shoot(hit.collider.gameObject, hit.point));
+                StartCoroutine(Shoot(hit.point));
             }
         }
 
         /// <summary>
-        /// Shoots the miniguns. Alternates between left and right, shooting from a
-        /// random barrel for aesthetic purposes. Every other shot from each minigun
-        /// will spawn a hit spark on the target. Periodically spawns a tracer to
-        /// give the player feedback as to where they are actually shooting.
+        /// Shoots the miniguns. Alternates between left and right, shooting from a random barrel
+        /// for aesthetic purposes. Spawns the muzzle flash emitter and plays the firing sound.
         /// </summary>
-        /// <param name="target"> The GameObject being shot </param>
         /// <param name="position"> The position of the shot </param>
-        private IEnumerator Shoot(GameObject target, Vector3 position)
+        private IEnumerator Shoot(Vector3 position)
         {
             canShoot = false;
-
-            source.PlayOneShot(minigunShootSound[0]);
 
             GameObject barrel = isLeftFire
                                     ? left[Random.Range(0, left.Length)]
                                     : right[Random.Range(0, right.Length)];
 
-            StartCoroutine(FlashShot(barrel, position, barrel.GetComponent<LineRenderer>()));
+            ParticleSystems muzzleFlash = muzzleFlashEmitters[muzzleFlashIndex++];
+            muzzleFlash.gameObject.transform.position = barrel.transform.position;
+            muzzleFlash.gameObject.transform.rotation = barrel.transform.rotation;
+            muzzleFlash.play();
+
+            if (muzzleFlashIndex >= muzzleFlashEmitters.Length)
+            {
+                muzzleFlashIndex = 0;
+            }
+
+            StartCoroutine(FlashTracer(barrel, position, barrel.GetComponent<LineRenderer>()));
+
+            source.PlayOneShot(minigunShootSound);
 
             reference.statistics.IsFiring();
             reference.statistics.WeaponFired("Minigun", 1);
@@ -157,8 +209,7 @@ namespace Hive.Armada.Player.Weapons
         /// <param name="barrel"> The barrel GameObject to start the LineRenderer at </param>
         /// <param name="position"> The position of the target to aim the LineRenderer at </param>
         /// <param name="tracer"> The LineRenderer itself </param>
-        private IEnumerator FlashShot(GameObject barrel, Vector3 position,
-                                      LineRenderer tracer)
+        private IEnumerator FlashTracer(GameObject barrel, Vector3 position, LineRenderer tracer)
         {
             tracer.endWidth = thickness *
                               Mathf.Max(Vector3.Magnitude(barrel.transform.position - position),
@@ -171,18 +222,6 @@ namespace Hive.Armada.Player.Weapons
             yield return new WaitForSeconds(0.006f);
 
             tracer.enabled = false;
-        }
-
-        /// <summary>
-        /// Calls the initialization of all LineRenderers
-        /// </summary>
-        protected override void SetupLineRenderers()
-        {
-            for (int i = 0; i < left.Length; ++i)
-            {
-                InitLineRenderer(left[i]);
-                InitLineRenderer(right[i]);
-            }
         }
 
         /// <summary>

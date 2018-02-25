76a42ee8ef970987bc6f2e3f63730c50c5ffe1e0
britt103 <britt103@mail.chapman.edu>
2018-02-25 01:09:47 -0800

    Add boss wave framework.

--- Hive Armada/Assets/Scripts/Game/BossWave.cs
+++ Hive Armada/Assets/Scripts/Game/BossWave.cs
@@ -1,5 +1,16 @@
-﻿using Hive.Armada.Enemies;
-using SubjectNerd.Utilities;
+﻿//=============================================================================
+//
+// Ryan Britton
+// 1849351
+// britt103@mail.chapman.edu
+// CPSC-340-01 & CPSC-344-01
+// Group Project
+//
+// Controls spawning of the Boss enemy throughout the game
+//
+//=============================================================================
+
+using Hive.Armada.Enemies;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -29,48 +40,67 @@ namespace Hive.Armada.Game
         /// </summary>
         private WaveManager waveManager;
 
-        private int enemiesRemaining;
+        /// <summary>
+        /// The index for the wave that is currently running.
+        /// </summary>
+        private int currentWave;
 
-        private int wave;
-
+        /// <summary>
+        /// The boss enemy.
+        /// </summary>
         public GameObject boss;
 
-        public GameObject backspawn;
+        /// <summary>
+        /// Reference for boss paths
+        /// </summary>
+        private GameObject bossSpawn;
+
+        /// <summary>
+        /// The health value of the boss.
+        /// </summary>
+        private int bossHealth;
 
         // Use this for initialization
         void Awake()
         {
             reference = GameObject.Find("Reference Manager").GetComponent<ReferenceManager>();
             waveManager = reference.waveManager;
+            bossSpawn = GameObject.Find("BossSpawn");
+            bossHealth = boss.GetComponent<Enemy>().Health;
+            Instantiate(boss, bossSpawn.transform);
+            boss.SetActive(false);
         }
 
 
         /// <summary>
         /// Runs the spawning logic for the wave.
         /// </summary>
+        /// <param name="wave"> This wave's index </param>
         public void Run(int wave)
         {
+            //placeholder for boss audio
             //source.PlayOneShot(clips[wave]);
             //yield return new WaitForSeconds(clips[wave].length);
-            backspawn = GameObject.Find("BackSpawn");
-            Instantiate(boss, backspawn.transform);
-            iTween.MoveTo(boss, iTween.Hash("path", iTweenPath.GetPath("BossIn") ,"easetype", iTween.EaseType.easeInOutSine,"time",5.0f,"looktarget",reference.playerShip.transform));          
-            //spawn and itween boss to path between front and back
+            currentWave = wave;            
+            boss.SetActive(true);
+            iTween.MoveTo(boss, iTween.Hash("path", iTweenPath.GetPath("BossIn") ,"easetype", iTween.EaseType.easeInOutSine,"time",5.0f,"looktarget",reference.playerShip.transform));
+            StartCoroutine(WaitForWaveEnd());
+
         }
 
         private IEnumerator WaitForWaveEnd()
         {
-            while (boss.GetComponent<Enemy>().Health >= 0)
+            while (bossHealth > 0)
             {
                 yield return new WaitForSeconds(0.1f);
             }
 
-            //Debug.Log("Wave #" + (wave + 1) + " is complete.");
+            Debug.Log("Wave #" + (currentWave + 1) + " is complete.");
 
             //play audio on defeat here?
             iTween.MoveTo(boss, iTween.Hash("path", iTweenPath.GetPath("BossOut"), "easetype", iTween.EaseType.easeInOutSine, "time", 5.0f, "looktarget", reference.playerShip.transform));
-
-            reference.waveManager.BossWaveComplete(wave);
+            boss.SetActive(false);
+            reference.waveManager.BossWaveComplete(currentWave);
         }
     }
     
